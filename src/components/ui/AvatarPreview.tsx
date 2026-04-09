4bdd4eb6b0b0428810a112079b19c8f0e7bb9be7
NimonokDanshi <aii.kmcryo@gmail.com>
2026-04-09 20:45:17 +0900

    city削除、エラーになってるかも。

--- src/components/ui/AvatarPreview.tsx
+++ src/components/ui/AvatarPreview.tsx
@@ -14,17 +14,16 @@ interface Props {
 export function AvatarPreview({ component: AvatarModel }: Props) {
   return (
     <div className="w-full h-full">
-      <Canvas 
-        shadows 
-        orthographic 
+      <Canvas
+        shadows
+        orthographic
         camera={{ position: [5, 5, 5], zoom: 40 }}
         gl={{ antialias: true, alpha: true }}
       >
         <Suspense fallback={null}>
           {/* Stageはモデルのサイズを計測して自動的にカメラと照明を調整してくれます */}
-          <Stage 
-            intensity={1.5} 
-            environment="city" 
+          <Stage
+            intensity={1.5}
             adjustCamera={1.2} // 1.2倍の余裕を持ってフィットさせる
             shadows="contact"
           >
